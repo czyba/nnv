3d0edfd930d9cf63f047cdadc4f99baf129624c6
czyba <czyba@embedded.rwth-aachen.de>
2015-12-22 21:18:29 +0000

    Fix formatting. Add a couple more defines to make life easier

--- fontoptions.h
+++ fontoptions.h
@@ -7,18 +7,21 @@
 #define FONT_BRIGHT_MASK 0xC
 #define FONT_UNDERSCORE_MASK 0x30
 #define FONT_BLINK_MASK 0xC0
+#define ANY_OPTION (FONT_BOLD_MASK | FONT_BRIGHT_MASK | FONT_UNDERSCORE_MASK | FONT_BLINK_MASK)
 
 enum FONT_OPTION {
-  FONT_DEFAULT =           0x0,
-  FONT_BOLD_ON =           0x1,
-  FONT_BOLD_OFF =          0x2,
-  FONT_HALF_BRIGHT_ON =    0x4,
-  FONT_HALF_BRIGHT_OFF =   0x8,
-  FONT_UNDERSCORE_ON =     0x10,
-  FONT_UNDERSCORE_OFF =    0x20,
-  FONT_BLINK_ON =          0x40,
-  FONT_BLINK_OFF =         0x80
+  FONT_DEFAULT =            0x00,
+  FONT_BOLD_ON =            0x01,
+  FONT_BOLD_OFF =           0x02,
+  FONT_BRIGHT_ON =          0x04,
+  FONT_BRIGHT_OFF =         0x08,
+  FONT_UNDERSCORE_ON =      0x10,
+  FONT_UNDERSCORE_OFF =     0x20,
+  FONT_BLINK_ON =           0x40,
+  FONT_BLINK_OFF =          0x80
 };
+
+#define IS_FONTOPTION(x) (x == 0 || (x & ANY_OPTION))
 
 enum FOREGROUND_COLORS {
   FG_BLACK =          30,
