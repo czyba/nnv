63d51b708bc6d26961ec74944763bcff3cc95878
czyba <czyba@embedded.rwth-aachen.de>
2015-12-25 22:49:35 +0000

    Refactor key parsing

--- input.c
+++ input.c
@@ -1,41 +1,47 @@
 #include "input.h"
 
+static key_t parse_CSI_3(unsigned char* a, size_t len) {
+  key_t key;
+  key.ascii = 0;
+  key.nkey = 0;
+  if(len > 4){
+    //TODO:
+  }
+  if(a[3] == '~') {
+    key.nkey = KEY_DEL;
+  }
+  return key;
+}
+
 static key_t parse_CSI(unsigned char* a, size_t len) {
   key_t key;
   key.ascii = 0;
   key.nkey = 0;
-  switch (a[len - 1]) {
+  if(len > 3) {
+    if (a[2] == '3') {
+      return parse_CSI_3(a, len);
+    }
+  }
+  switch (a[2]) {
   case 'A':
   case 'B':
   case 'C':
   case 'D': {
     key.nkey = a[len - 1] - 'A' + 1;
-    if (len != 3) {
-      if (a[len - 2] == '1') {
-        key.nkey |= MOD_RALT;
-      } else if (a[len - 2] == '3') {
-        key.nkey |= MOD_LALT;
-      } else if (a[len - 2] == '5') {
-        key.nkey |= MOD_CTRL;
-      }
-    }
     return key;
   }
   case 'F': {
     key.nkey = KEY_END;
-    return key;
+    break;
   }
   case 'H': {
     key.nkey = KEY_HOME;
-    return key;
+    break;
   }
   default:
     break;
   }
-  if (len == 4 && a[2] == '3' && a[3] == '~') {
-    //DEL
-    key.nkey = KEY_DEL;
-  }
+  return key;
   return key;
 }
 
